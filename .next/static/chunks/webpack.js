8d552699271a23af463451ac1c592041a59e1a82
miroujad <aamirelmot@gmail.com>
2025-10-20 11:27:33 +0200

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a5c718ceec04eb6"; }
+/******/ 		__webpack_require__.h = function() { return "1e0a6789f5171329"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
